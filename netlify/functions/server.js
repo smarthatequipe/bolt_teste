e95c4281368f40330a16d81632a28ffac10593cd
Leonardo-Santos-oficial <seu.email@exemplo.com>
2025-03-20 15:56:00 -0300

    Corrigir importação do build no arquivo de função serverless do Netlify

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,5 +1,5 @@
 import { createRequestHandler } from "@remix-run/netlify";
-import * as build from "@remix-run/dev/server-build";
+import * as build from "../../build/server/index.js";
 
 export const handler = createRequestHandler({
   build,
